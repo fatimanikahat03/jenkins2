f53632d2de6b10a38fb071bd9fe0d30bbec23b90
fatimanikahat03 <133115566+fatimanikahat03@users.noreply.github.com>
2023-08-25 01:45:57 -0400

    rrrrrrrrrrrrrrrrrrrrrrrrrrrrrrrrrrrrrrrr

--- firstjenkin/src/test/java/TestRunner/MyStepdefs.java
+++ firstjenkin/src/test/java/TestRunner/MyStepdefs.java
@@ -25,7 +25,9 @@ public class MyStepdefs {
     @Given("Enter URL in chrome Browser")
     public void enterURLInChromeBrowser() throws IOException, InterruptedException {
 //        driver = BrowserCall.Browserinvocation(); //facebook.com  // implicit timeout enable for execution or sleep method
-        Thread.sleep(2000);
+       // Thread.sleep(2000);
+        .System.setProperty("webdriver.edge.driver", "C:\\Program Files (x86)\\Microsoft\\Edge\\Application\\msedgedriver.exe");
+
 EdgeOptions edgeOptions=new EdgeOptions();
        // System.setProperty("webdriver.edge.verboseLogging", "true");
 
